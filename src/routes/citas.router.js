8672b8435d764d638b82d6834709c8ea4d8e96fa
ah25001 <ah25001@esfe.agape.edu.sv>
2025-09-28 04:37:50 +0000

    Cambios en permisos para citas

--- src/routes/citas.router.js
+++ src/routes/citas.router.js
@@ -4,10 +4,10 @@ const controller = require('../controllers/citas.controller');
 const { verifyToken, onlyCliente, onlyTrabajador, onlyAdmin } = require('../middlewares/auth');// Importa el middleware de autenticación
 
 router.get('/', verifyToken, controller.index);
-router.get('/',verifyToken, onlyAdmin, onlyTrabajador, onlyCliente, controller.index);       // Listar todas las citas
-router.get('/:id',verifyToken, onlyTrabajador, onlyCliente, onlyAdmin, controller.show);     // Obtener una cita por ID
-router.post('/',verifyToken, onlyAdmin, onlyCliente, controller.store);      // Crear una nueva cita
-router.put('/:id',verifyToken, onlyAdmin, onlyTrabajador, onlyCliente, controller.update);   // Actualizar una cita
-router.delete('/:id',verifyToken, onlyAdmin, onlyCliente, onlyTrabajador, controller.destroy); // Eliminar una cita
+router.get('/',verifyToken, onlyAdmin, onlyTrabajador, controller.index);       // Listar todas las citas
+router.get('/:id',verifyToken, onlyTrabajador, onlyAdmin, controller.show);     // Obtener una cita por ID
+router.post('/',verifyToken, onlyCliente, controller.store);      // Crear una nueva cita
+router.put('/:id',verifyToken, onlyAdmin, onlyTrabajador, controller.update);   // Actualizar una cita
+router.delete('/:id',verifyToken, onlyAdmin, onlyTrabajador, controller.destroy); // Eliminar una cita
 
 module.exports = router;
